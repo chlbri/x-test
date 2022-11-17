e32a6c5637223654636ba8108cd4c0ea41674b1f
chlbri <bri_lvi@icloud.com>
2022-11-17 17:19:21 +0000

    fix((matches)): Add non nullable parameter Author: chlbri <bri_lvi@icloud.com>

--- src/matches.ts
+++ src/matches.ts
@@ -9,7 +9,7 @@ export type MatchOptions<T extends string = string> =
 
 function buildMatches(
   decomposeds: readonly string[],
-  value: MatchOptions
+  value: MatchOptions,
 ): boolean {
   let out = false;
   if (typeof value === 'string') {
@@ -17,27 +17,22 @@ function buildMatches(
   } else if ('or' in value) {
     const _values = value.or;
     out = _values
-      .map((value) => buildMatches(decomposeds, value))
-      .some((value) => value === true);
+      .map(value => buildMatches(decomposeds, value))
+      .some(value => value === true);
   } else {
     const _values = value.and;
     out = _values
-      .map((value) => buildMatches(decomposeds, value))
-      .every((value) => value === true);
+      .map(value => buildMatches(decomposeds, value))
+      .every(value => value === true);
   }
 
   return out;
 }
 
-export function matches<T extends StateValue = StateValue>(value?: T) {
-  if (!value) {
-    return (..._: MatchOptions<StateMatching<T>>[]) => false;
-  }
+export function matches<T extends StateValue = StateValue>(value: T) {
   const decomposeds = decompose(value);
   return (...values: MatchOptions<StateMatching<T>>[]) => {
-    const matchers = values.map((value) =>
-      buildMatches(decomposeds, value)
-    );
-    return matchers.every((matcher) => matcher === true);
+    const matchers = values.map(value => buildMatches(decomposeds, value));
+    return matchers.every(matcher => matcher === true);
   };
 }
